46eae5caae193531f63ca51e07c2f0a1d730ac28
Giuseppe Mumolo <giuseppe.mumolo@avanade.com>
2021-09-13 22:17:59 +0200

    aggiunti bottoni

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // constructor
-  const MyApp({Key? key}) : super(key: key);
+  var questions = [
+    'What\'s your favorite color?',
+    'What\'s your favorite animal?'
+  ];
+
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      // home is the widget for the default route of the app ([Navigator.defaultRouteName], which is / ).
-      home: Scaffold(
-          appBar: AppBar(title: const Text('My first App')),
-          body: const Text('This is my default body')),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        // home is the widget for the default route of the app ([Navigator.defaultRouteName], which is / ).
+        home: Scaffold(
+            appBar: AppBar(title: const Text('My first App')),
+            body: Column(
+              children: [
+                const Text('The question!'),
+                ElevatedButton(
+                    child: const Text('Answer 1'), onPressed: onPressed),
+                ElevatedButton(
+                    child: const Text('Answer 2'),
+                    onPressed: () {
+                      print('Answer 2 chosen!');
+                    }),
+                ElevatedButton(
+                    child: const Text('Answer 3'),
+                    onPressed: () => print('Answer 3 chsoen!')),
+              ],
+            )));
   }
+
+  onPressed() => print('Answer chosen!');
 }
+
