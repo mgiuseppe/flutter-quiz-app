172709524b1b5f0825b8e48f3f2d24e544daedda
Giuseppe Mumolo <giuseppe.mumolo@avanade.com>
2021-09-14 17:12:08 +0200

    styling question with Container

--- lib/question.dart
+++ lib/question.dart
@@ -7,6 +7,13 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+        width: double.infinity,
+        margin: const EdgeInsets.all(10),
+        child: Text(
+          questionText,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 28),
+        ));
   }
 }
